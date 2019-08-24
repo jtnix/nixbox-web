f79517c74b915911cf2ae22d54b311c5dc4abc17
Jolyon Terwilliger <jolyon@nixbox.com>
2019-08-24 01:52:07 -0400

    Amplify update (#1)
    * adds netlify + dynamic lambda test
    
    * amplify login
    
    * adds analytics and login fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,67 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import React from "react";
+import { Link } from "gatsby";
+import github from "../img/github-icon.svg";
+import logo from "../img/logo.svg";
+import md5 from "md5";
+import { navigate } from "@reach/router";
+import Gravatar from "react-gravatar";
+import { getCurrentUser, setUser } from "../utils/auth";
+import Amplify, { Auth } from "aws-amplify";
+import config from "../aws-exports";
+Amplify.configure(config);
 
 const Navbar = class extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       active: false,
-      navBarActiveClass: '',
-    }
+      navBarActiveClass: "",
+      user: null
+    };
   }
+
+  componentDidMount = () => {
+    const user = getCurrentUser();
+    this.setState({ user });
+  };
+
+  componentDidUpdate = (prevProps, prevState) => {
+    const user = getCurrentUser();
+    if (this.state.user.email !== user.email) {
+      // console.log(this.state, user);
+      this.setState({ user });
+    }
+  };
 
   toggleHamburger = () => {
     // toggle the active boolean in the state
     this.setState(
       {
-        active: !this.state.active,
+        active: !this.state.active
       },
       // after state has been updated,
       () => {
         // set the class in state for the navbar accordingly
         this.state.active
           ? this.setState({
-              navBarActiveClass: 'is-active',
+              navBarActiveClass: "is-active"
             })
           : this.setState({
-              navBarActiveClass: '',
-            })
+              navBarActiveClass: ""
+            });
       }
-    )
-  }
+    );
+  };
+
+  logout = () => {
+    Auth.signOut();
+    this.setState({ user: {} });
+    setUser({});
+    navigate("/");
+  };
 
   render() {
+    const { user } = this.state;
     return (
       <nav
         className="navbar is-transparent"
@@ -42,7 +71,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <img src={logo} alt="Kaldi" style={{ width: "88px" }} />
             </Link>
             {/* Hamburger menu */}
             <div
@@ -77,6 +106,28 @@ const Navbar = class extends React.Component {
               </Link>
             </div>
             <div className="navbar-end has-text-centered">
+              <div className="userMenu navbar-item">
+                {user && user.email ? (
+                  <>
+                    <a>
+                      <span className="icon">
+                        <Gravatar className="circle" md5={md5(user.email)} />
+                      </span>
+                      <span className="nav-greet">Hi, {user.username}</span>
+                    </a>
+                    <div className="userDropdown">
+                      <p>
+                        <Link to="/user/profile">Profile</Link>
+                      </p>
+                      <p>
+                        <a onClick={this.logout}>Log out</a>
+                      </p>
+                    </div>
+                  </>
+                ) : (
+                  <Link to="/user/login">Sign In</Link>
+                )}
+              </div>
               <a
                 className="navbar-item"
                 href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
@@ -91,8 +142,8 @@ const Navbar = class extends React.Component {
           </div>
         </div>
       </nav>
-    )
+    );
   }
-}
+};
 
-export default Navbar
+export default Navbar;
---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { navigate, Redirect } from "@reach/router";
+import { isLoggedIn } from "../utils/auth";
+
+class PrivateRoute extends React.Component {
+  render() {
+    const { component: Component, location, ...rest } = this.props;
+    if (!isLoggedIn()) {
+      console.log(location);
+      // using Redirect with noThrow instead of imperative navigate to catch first render redirects
+      return <Redirect from={location.pathname} to="/user/login" noThrow />;
+      // navigate(`/user/login`, { replace: true });
+      // return null;
+    }
+    return (
+      <div>
+        <h2>Private</h2>
+        <Component {...rest} />
+      </div>
+    );
+  }
+}
+
+export default PrivateRoute;
