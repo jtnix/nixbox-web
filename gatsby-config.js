497bba4070ace2b188845ceb5660df9e94df011a
Jolyon Terwilliger <jolyon@nixbox.com>
2019-08-25 01:25:34 -0400

    reenable cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -142,12 +142,12 @@ module.exports = {
         // cookieDomain: "localhost"
       }
     },
-    // {
-    //   resolve: "gatsby-plugin-netlify-cms",
-    //   options: {
-    //     modulePath: `${__dirname}/src/cms/cms.js`
-    //   }
-    // },
+    {
+      resolve: "gatsby-plugin-netlify-cms",
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`
+      }
+    },
     {
       resolve: "gatsby-plugin-purgecss", // purges all unused/unreferenced css rules
       options: {
